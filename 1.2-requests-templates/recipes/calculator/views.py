53a82bb0fa3ef8bc0a51bf3087e5b455acc245e9
oligo <oligo83@gmail.com>
2024-04-09 02:04:21 +0700

    Initial commit.

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import reverse
 
 DATA = {
     'omlet': {
@@ -16,7 +18,11 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
+    'hatchet_porridge': {
+        'крупа, стакан': 1,
+        'масло, ломтик': 1,
+        'топор, шт': 1,
+    },
 }
 
 # Напишите ваш обработчик. Используйте DATA как источник данных
@@ -28,3 +34,37 @@ DATA = {
 #     'ингредиент2': количество2,
 #   }
 # }
+
+def menu_view(request: HttpRequest) -> HttpResponse:
+    template_name = 'menu.html'
+    context = {'menu': DATA,
+               'dish_page': reverse('dish')}
+    msg = render(request, template_name, context=context)
+    return msg
+
+def recipe_view(request: HttpRequest) -> HttpResponse:
+    template_name = 'recipe.html'
+    dish = request.GET.get('dish')
+    context = {'recipe': DATA[dish],
+               'dish': dish
+    }
+    msg = render(request, template_name, context=context)
+    return msg
+
+def result_view(request: HttpRequest) -> HttpResponse:
+    template_name = 'result.html'
+    try:
+        persons = int(request.POST.get('qtty', '0'))
+    except:
+        persons = 0
+
+    url = str(request)
+    dish = url[url.find('?dish=')+6: len(url)-2]
+
+    result = {}
+    for name, qtty in DATA[dish].items():
+        result[name] = qtty * persons
+    context = {'result': result}
+    return render(request, template_name, context=context)
+
+
